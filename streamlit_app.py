cc63f6f3b0f1795e0ff6ea599fe726981297e934
bagozalan <92088627+bagozalan@users.noreply.github.com>
2023-05-31 14:04:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,12 +15,7 @@ def main():
         fig, ax = plot_confusion_matrix(conf_mat=cm)
         st.pyplot(fig)
 
-        # Classification report létrehozása
-        report = classification_report(Y_test, model.predict(X_test))
-        precision = report['1']['precision']
-        recall = report['1']['recall']
-        st.write('RandomForest precision:', precision)
-        st.write('RandomForest recall:', recall)
+
 
         accuracy = model.score(X_test, Y_test)
         st.write('RandomForest pontossága:', accuracy)
@@ -33,12 +28,8 @@ def main():
         fig, ax = plot_confusion_matrix(conf_mat=cm)
         st.pyplot(fig)
 
-        # Classification report létrehozása
-        report2 = classification_report(Y_test, model2.predict(X_test))
-        precision = report2['1']['precision']
-        recall = report2['1']['recall']
-        st.write('DecisionTreeClassifier precision:', precision)
-        st.write('DecisionTreeClassifier recall:', recall)
+
+
 
         accuracy2 = model2.score(X_test, Y_test)
         st.write('DecisionTreeClassifier pontossága:', accuracy2)
@@ -52,11 +43,8 @@ def main():
         st.pyplot(fig)
 
         # Classification report létrehozása
-        report3 = classification_report(Y_test, model3.predict(X_test))
-        precision = report3['1']['precision']
-        recall = report3['1']['recall']
-        st.write('BaggingClassifier precision:', precision)
-        st.write('BaggingClassifier recall:', recall)
+        
+
 
         accuracy3 = model3.score(X_test, Y_test)
         st.write('BaggingClassifier pontossága:', accuracy3)
