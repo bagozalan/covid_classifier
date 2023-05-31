0d76305be0289b65259e274cfd7276f9da212a08
bagozalan <92088627+bagozalan@users.noreply.github.com>
2023-05-31 14:11:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from covid_classifier import model, model2, model3
 from covid_classifier import X_test, Y_test
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report, recall_score, f1_score
 from mlxtend.plotting import plot_confusion_matrix
 
 def main():
@@ -15,11 +15,12 @@ def main():
         fig, ax = plot_confusion_matrix(conf_mat=cm)
         st.pyplot(fig)
 
-
+        f1_score = f1_score(Y_test, model.predict(X_test), pos_label=1)
         recall = recall_score(Y_test, model.predict(X_test), pos_label=1)
         accuracy = model.score(X_test, Y_test)
         st.write('RandomForest score:', accuracy)
         st.write('RandomForest recall:', recall)
+        st.write('RandomForest f1_score:', f1_score)
     
     if st.button('DecisionTreeClassifier'):
         # Confusion matrix létrehozása
@@ -31,11 +32,12 @@ def main():
 
 
         recall2 = recall_score(Y_test, model2.predict(X_test), pos_label=1)
-       
+        f1_score2 = f1_score(Y_test, model2.predict(X_test), pos_label=1)
 
         accuracy2 = model2.score(X_test, Y_test)
         st.write('DecisionTreeClassifier score:', accuracy2)
         st.write('DecisionTreeClassifier recall:', recall2)
+        st.write('DecisionTreeClassifier recall:', f1_score2)
     
     if st.button('BaggingClassifier'):
         # Confusion matrix létrehozása
@@ -47,11 +49,12 @@ def main():
 
         # Classification report létrehozása
         
-
+        f1_score3 = f1_score(Y_test, model3.predict(X_test), pos_label=1)
         recall3 = recall_score(Y_test, model3.predict(X_test), pos_label=1)
         accuracy3 = model3.score(X_test, Y_test)
         st.write('BaggingClassifier score:', accuracy3)
         st.write('DecisionTreeClassifier recall:', recall3)
+        st.write('DecisionTreeClassifier recall:', f1_score3)
 
 
 if __name__ == '__main__':
