a49fdca12c159e951d5490128f731f0746d5256b
bagozalan <92088627+bagozalan@users.noreply.github.com>
2023-05-31 14:13:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,12 +15,12 @@ def main():
         fig, ax = plot_confusion_matrix(conf_mat=cm)
         st.pyplot(fig)
 
-        f1_score = f1_score(Y_test, model.predict(X_test), pos_label=1)
+        f1 = f1_score(Y_test, model.predict(X_test), pos_label=1)
         recall = recall_score(Y_test, model.predict(X_test), pos_label=1)
         accuracy = model.score(X_test, Y_test)
         st.write('RandomForest score:', accuracy)
         st.write('RandomForest recall:', recall)
-        st.write('RandomForest f1_score:', f1_score)
+        st.write('RandomForest F1 score:', f1)
     
     if st.button('DecisionTreeClassifier'):
         # Confusion matrix létrehozása
@@ -30,14 +30,13 @@ def main():
         fig, ax = plot_confusion_matrix(conf_mat=cm)
         st.pyplot(fig)
 
-
         recall2 = recall_score(Y_test, model2.predict(X_test), pos_label=1)
         f1_score2 = f1_score(Y_test, model2.predict(X_test), pos_label=1)
 
         accuracy2 = model2.score(X_test, Y_test)
         st.write('DecisionTreeClassifier score:', accuracy2)
         st.write('DecisionTreeClassifier recall:', recall2)
-        st.write('DecisionTreeClassifier recall:', f1_score2)
+        st.write('DecisionTreeClassifier F1 score:', f1_score2)
     
     if st.button('BaggingClassifier'):
         # Confusion matrix létrehozása
@@ -47,14 +46,12 @@ def main():
         fig, ax = plot_confusion_matrix(conf_mat=cm)
         st.pyplot(fig)
 
-        # Classification report létrehozása
-        
         f1_score3 = f1_score(Y_test, model3.predict(X_test), pos_label=1)
         recall3 = recall_score(Y_test, model3.predict(X_test), pos_label=1)
         accuracy3 = model3.score(X_test, Y_test)
         st.write('BaggingClassifier score:', accuracy3)
-        st.write('DecisionTreeClassifier recall:', recall3)
-        st.write('DecisionTreeClassifier recall:', f1_score3)
+        st.write('BaggingClassifier recall:', recall3)
+        st.write('BaggingClassifier F1 score:', f1_score3)
 
 
 if __name__ == '__main__':
